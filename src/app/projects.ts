9a6acedc17298370f23052f3c80b3bc3a74d9d97
txnio <bytxnio@gmail.com>
2026-08-28 18:54:51 +0200

    Link to standalone VGPU Lab

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -1,5 +1,6 @@
 export const projects = [
   { title: 'Tresdé', group: 'apps', href: 'https://tresde.txnio.com' },
   { title: 'txniOS', group: 'apps', href: 'https://os.txnio.com' },
+  { title: 'VGPU Lab', group: 'lab', href: 'https://vgpulab.txnio.com' },
   { title: 'Minder', group: 'apps', href: 'https://minder-txnio.vercel.app/' },
 ] as const;
